46ec39867a6d08a2e55fbd6ac1bfe77a303a5814
Johnpel <johnpelquingua@gmail.com>
2026-09-26 22:23:49 +0800

    Use standard Google tag with strict CSP hash

--- google-tag.js
+++
@@ -1,6 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-window.gtag = function () {
-  window.dataLayer.push(arguments);
-};
-window.gtag('js', new Date());
-window.gtag('config', 'G-GZWY3EYCE1');
