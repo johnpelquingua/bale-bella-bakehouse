88a92c31de33d02cc3d58a9152384857c917904a
Johnpel <johnpelquingua@gmail.com>
2026-09-26 22:16:04 +0800

    Fix GA4 tracking under strict CSP

---
+++ google-tag.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = function () {
+  window.dataLayer.push(arguments);
+};
+window.gtag('js', new Date());
+window.gtag('config', 'G-GZWY3EYCE1');
